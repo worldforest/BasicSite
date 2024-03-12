aecc5c2fbade7d4fc3f6cfe5d87e37f946643976
serim.jaen <serim.jaen@androidjava.com>
2024-03-12 09:31:54 +0900

    add test file

---
+++ src/main/java/com/basic/basicsite/BasicSiteApplication.java
@@ -0,0 +1,13 @@
+package com.basic.basicsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basic/basicsite/BasicSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic.basicsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
